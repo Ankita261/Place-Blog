f46d50e901478a673315eb0467bc5fb6d5c4c5be
Ankita <ankita.mandal2611@gmail.com>
2022-05-29 03:47:00 +0530

    file upload added for user signup and new places

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -9,6 +11,8 @@ const HttpError = require('./models/http-error');
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,6 +34,11 @@ app.use((req, res, next) => {
 });
 
 app.use((error, req, res, next) => {
+  if(req.file){
+    fs.unlink(req.file.path, () => {
+      console.log(err);
+    });
+  }
   if (res.headerSent) {
     return next(error);
   }
@@ -39,7 +48,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-  .connect(`MONGO_URI`)
+  .connect(`mongodb+srv://placeblog:placeblog@cluster0.7ed0a.mongodb.net/blog?retryWrites=true&w=majority`)
   .then(() => {
     app.listen(5000);
   })
